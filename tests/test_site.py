d6e2bb4c644a4e53be6a9e47df4f31c9936720fa
Miguel Dias <miguel.dias.1212@gmail.com>
2025-03-28 17:36:37 +0100

    test: add basic server accessibility test

--- tests/test_site.py
+++ tests/test_site.py
@@ -1,1 +1,8 @@
- 
+import requests
+import pytest
+
+def test_server_is_running():
+    """Test that the server is running and accessible."""
+    response = requests.get('http://localhost:4444')
+    assert response.status_code == 200
+    assert 'text/html' in response.headers['content-type'] 
