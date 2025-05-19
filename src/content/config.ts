c621a896e4dc3964135012aaf405fe71e7dfb537
Miguel Dias <miguel.dias.1212@gmail.com>
2025-05-19 16:15:08 +0200

    Modified files: src/content/config.ts,src/pages/blog/index.astro,

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,6 +7,7 @@ const posts = defineCollection({
     description: z.string(),
     date: z.string(),
     image: z.string().startsWith('/'),
+    draft: z.boolean().optional().default(false),
   }),
 });
 
